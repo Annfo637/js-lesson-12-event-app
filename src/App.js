f9ace92ef68253be2caaf56c3a55abb62b69d382
Annfo637 <54262041+Annfo637@users.noreply.github.com>
2020-09-10 11:34:00 +0200

    Start with login logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 
+const ROOT_URL = "http://yoshi.willandskill.eu:8999/api/v1/";
+const LOGIN_URL = `${ROOT_URL}auth/api-token-auth/`;
+
 function App() {
   function login() {
-    console.log("login");
+    fetch(LOGIN_URL);
   }
 
   return (
