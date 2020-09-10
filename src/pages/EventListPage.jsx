5c09a5dc0e16725a3706af89fdb73a9affb31946
Annfo637 <54262041+Annfo637@users.noreply.github.com>
2020-09-10 15:11:01 +0200

    Add FetchEventList and useEffect to render

--- src/pages/EventListPage.jsx
+++ src/pages/EventListPage.jsx
@@ -1,8 +1,24 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { UserContext } from "../contexts/UserContext";
+
+const ROOT_URL = "http://yoshi.willandskill.eu:8999/api/v1/";
+const EVENT_LIST_URL = `${ROOT_URL}events/events/`;
 
 export default function EventListPage() {
   const { token } = useContext(UserContext);
+
+  useEffect(() => {
+    fetchEventList();
+  }, []);
+
+  function fetchEventList() {
+    fetch(EVENT_LIST_URL, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  }
   return (
     <div>
       <h1>EventList Page</h1>
