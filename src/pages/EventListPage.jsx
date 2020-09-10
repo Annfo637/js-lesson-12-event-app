50167b40e19c37c0a5f85d2b5f11dcb26d1990c9
Annfo637 <54262041+Annfo637@users.noreply.github.com>
2020-09-10 15:27:46 +0200

    Set response to state variabel and render eventlist

--- src/pages/EventListPage.jsx
+++ src/pages/EventListPage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { UserContext } from "../contexts/UserContext";
 
 const ROOT_URL = "http://yoshi.willandskill.eu:8999/api/v1/";
@@ -6,6 +6,7 @@ const EVENT_LIST_URL = `${ROOT_URL}events/events/`;
 
 export default function EventListPage() {
   const { token } = useContext(UserContext);
+  const [eventList, setEventList] = useState(null);
 
   useEffect(() => {
     fetchEventList();
@@ -17,11 +18,26 @@ export default function EventListPage() {
         "Content-Type": "application/json",
         Authorization: `Bearer ${token}`,
       },
-    });
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setEventList(data.results);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
   return (
     <div>
       <h1>EventList Page</h1>
+      {eventList &&
+        eventList.map((eventItem) => {
+          return (
+            <div key={eventItem.id}>
+              <p>{eventItem.title}</p>
+            </div>
+          );
+        })}
       {token}
     </div>
   );
